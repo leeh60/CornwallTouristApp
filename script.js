3e9ec852e1103868b60703ef940cc3d21aa9ec3c
Lee <leebee1234@hotmail.co.uk>
2017-05-05 00:28:24 +0100

    added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ﻿if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
 
     console.log('Service worker registered successfully');
   }).catch(function(err) {
