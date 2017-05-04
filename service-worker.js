94e94db42f769c7e9174038a264dd0a2cc0e34fd
Lee <leebee1234@hotmail.co.uk>
2017-05-05 00:35:39 +0100

    added

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('gih-cache').then(function(cache) {
+      return cache.add('index.html');
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match('index.html');
+    })
+  );
+});
+
