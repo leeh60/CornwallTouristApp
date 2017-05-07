f7ea4504d1aa579d083f24a15ccab4849cc35e85
Lee <leebee1234@hotmail.co.uk>
2017-05-07 03:24:45 +0100

    added

--- script.js
+++ script.js
@@ -6,3 +6,20 @@
     console.log('Service worker registration failed: ', err);
   });
 }
+
+var burgerButton = document.getElementById("hamburger-button"),
+    burgerMenu = document.getElementById("hamburger-menu"),
+    burgerCloseButton = document.getElementById("hamburger-button-close");
+	container = document.getElementById("container");
+
+burgerButton.addEventListener("click", burgerClicked, false);
+burgerCloseButton.addEventListener("click", burgerClicked, false);
+
+function burgerClicked(e){
+  e.preventDefault();
+  if(container.classList.contains("burger-open")){
+    container.classList.remove("burger-open");
+  }else{
+    container.classList.add("burger-open");  
+  }
+}
